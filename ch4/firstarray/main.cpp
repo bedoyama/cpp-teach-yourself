5fe65f4c0d255c9c74575acf439564cfdc4abb15
Mauricio Bedoya <bedoyama@gmail.com>
2019-02-18 15:17:07 -0800

    first array

---
+++ ch4/firstarray/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myNumbers[5] = {34, 56, -21, 5002, 365};
+
+    cout << "Element at index 0: " << myNumbers[0] << endl;
+    cout << "Element at index 1: " << myNumbers[1] << endl;
+    cout << "Element at index 2: " << myNumbers[2] << endl;
+    cout << "Element at index 3: " << myNumbers[3] << endl;
+    cout << "Element at index 4: " << myNumbers[4] << endl;
+
+    return 0;
+}
