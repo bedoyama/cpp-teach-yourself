9d375c9e8a9598cad7d72fac54027458469330c4
bedoyama <bedoyama@gmail.com>
2019-02-18 11:52:11 -0800

    int overflow 2

--- ch3/integeroverflow/main.cpp
+++ ch3/integeroverflow/main.cpp
@@ -10,5 +10,22 @@ int main() {
     cout << "Incrementing signed short " << sShortValue << " gives: ";
     cout << ++sShortValue << endl;
 
+    // int and long int are the same
+    unsigned int uIntValue = 4294967295;
+    cout << "Incrementing unsigned int " << uIntValue << " gives: ";
+    cout << ++uIntValue << endl;
+
+    int sIntValue = 2147483647;
+    cout << "Incrementing signed int " << sIntValue << " gives: ";
+    cout << ++sIntValue << endl;
+
+    unsigned long long int uLongIntValue = 18446744073709551615;
+    cout << "Incrementing unsigned long long int " << uLongIntValue << " gives: ";
+    cout << ++uLongIntValue << endl;
+
+    long long int sLongIntValue = 9223372036854775807;
+    cout << "Incrementing signed long long int " << sLongIntValue << " gives: ";
+    cout << ++sLongIntValue << endl;
+
     return 0;
 }
